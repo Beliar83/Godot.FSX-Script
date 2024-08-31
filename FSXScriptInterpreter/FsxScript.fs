102638f3b6dcf8897d45294bcc3a42f562dcf061
Karsten Bock <KarstenBock@gmx.net>
2024-08-31 08:36:48 +0200

    Return explicit default values

--- FSXScriptInterpreter/FsxScript.fs
+++ FSXScriptInterpreter/FsxScript.fs
@@ -3,116 +3,148 @@ namespace FSXScriptInterpreter
 open FSXScriptInterpreter.FsxScriptInstance
 open Godot
 open Godot.Bridge
+open Godot.Collections
 
 type FsxScript() as this =
     inherit ScriptExtension()
-    let mutable sourceCode : string = null    
-    
+    let mutable sourceCode: string = null
+
     static member val LanguageName = new StringName("FsxScriptLanguage")
     static member val ClassName = new StringName(nameof FsxScript)
 
     static member BindMethods(context: ClassDBRegistrationContext) =
         context.BindConstructor(fun () -> new FsxScript())
-    override _._InstanceCreate(forObject : GodotObject) =
+
+    override _._InstanceCreate(forObject: GodotObject) =
         Godot.Extensions.Script.CreateInstance<FsxScriptInstance>(forObject, this)
+
     override _._GetLanguage() =
         GD.Print("_GetLanguage")
         Engine.Singleton.GetSingleton(FsxScript.LanguageName) :?> ScriptLanguage
+
     override _._CanInstantiate() =
         GD.Print("CanInstantiate")
-        base._CanInstantiate()    
+        false
+
     override _._EditorCanReloadFromFile() =
         GD.Print("CanInstantiate")
-        base._EditorCanReloadFromFile()    
+        false
+
     override _._GetBaseScript() =
         GD.Print("GetBaseScript")
-        base._GetBaseScript()    
+        null
+
     override _._GetClassIconPath() =
         GD.Print("GetClassIconPath")
-        base._GetClassIconPath()    
+        null
+
     override _._GetConstants() =
         GD.Print("GetConstants")
-        base._GetConstants()    
+        new GodotDictionary()
+
     override _._GetDocumentation() =
         GD.Print("GetDocumentation")
-        base._GetDocumentation()
+        new GodotArray<GodotDictionary>()
+
     override _._GetGlobalName() =
         GD.Print("GetGlobalName")
-        base._GetGlobalName()    
+        null
+
     override _._GetInstanceBaseType() =
         GD.Print("GetInstanceBaseType")
-        base._GetInstanceBaseType()            
+        new StringName("Object")
+
     override _._GetMemberLine(_member) =
         GD.Print("GetMemberLine")
-        base._GetMemberLine(_member)
+        0
+
     override _._GetMembers() =
         GD.Print("GetMembers")
-        base._GetMembers()
+        new GodotArray<StringName>()
+
     override _._GetMethodInfo(method) =
         GD.Print("GetMethodInfo")
-        base._GetMethodInfo(method)                
+        new GodotDictionary()
+
     override _._GetPropertyDefaultValue(property) =
         GD.Print("GetPropertyDefaultValue")
-        base._GetPropertyDefaultValue(property)
+        new Variant()
+
     override _._GetRpcConfig() =
         GD.Print("GetRpcConfig")
-        base._GetRpcConfig()        
+        new Variant()
+
     override _._GetScriptMethodArgumentCount(method) =
         GD.Print("GetScriptMethodArgumentCount")
-        base._GetScriptMethodArgumentCount(method)
+        new Variant()
+
     override _._GetScriptMethodList() =
         GD.Print("GetScriptMethodList")
-        base._GetScriptMethodList()
+        new GodotArray<GodotDictionary>()
+
     override _._GetScriptPropertyList() =
         GD.Print("GetScriptPropertyList")
-        base._GetScriptPropertyList()
+        new GodotArray<GodotDictionary>()
+
     override _._GetScriptSignalList() =
         GD.Print("GetScriptSignalList")
-        base._GetScriptSignalList()  
+        new GodotArray<GodotDictionary>()
+
     override _._PlaceholderErased(placeholder) =
         GD.Print("PlaceholderErased")
-        base._PlaceholderErased(placeholder)
+        ()
+
     override _._InheritsScript(script) =
         GD.Print("InheritsScript")
-        base._InheritsScript(script)
+        false
+
     override _._PlaceholderInstanceCreate(forObject) =
-        Godot.Extensions.Script.CreateInstance<FsxScriptInstance>(forObject, this, true)        
+        Godot.Extensions.Script.CreateInstance<FsxScriptInstance>(forObject, this, true)
+
     override _._InstanceHas(object) =
         GD.Print("_InstanceHas")
-        base._InstanceHas(object)
-    override _._HasSourceCode() =
-        sourceCode <> null
-    override _._GetSourceCode() =
-        sourceCode        
-    override _._SetSourceCode(code) =
-        sourceCode <- code
+        false
+
+    override _._HasSourceCode() = sourceCode <> null
+    override _._GetSourceCode() = sourceCode
+    override _._SetSourceCode(code) = sourceCode <- code
+
     override _._Reload(keepState) =
         GD.Print("_Reload")
-        base._Reload(keepState)
+        Error.Unavailable
+
     override _._HasMethod(method) =
         GD.Print("_HasMethod")
-        base._HasMethod(method)
+        false
+
     override _._HasStaticMethod(method) =
         GD.Print("_HasStaticMethod")
-        base._HasStaticMethod(method)
+        false
+
     override _._IsTool() =
         GD.Print("_IsTool")
-        base._IsTool()
+        false
+
     override _._IsValid() =
         GD.Print("_IsValid")
-        base._IsValid()
+        false
+
     override _._IsAbstract() =
         GD.Print("_IsAbstract")
-        base._IsAbstract()
+        false
+
     override _._HasScriptSignal(signal) =
         GD.Print("_HasScriptSignal")
-        base._HasScriptSignal(signal)
+        false
+
     override _._HasPropertyDefaultValue(property) =
         GD.Print("_HasPropertyDefaultValue")
-        base._HasPropertyDefaultValue(property)
+        false
+
     override _._UpdateExports() =
         GD.Print("_UpdateExports")
-        base._UpdateExports()
+        ()
+
     override _._IsPlaceholderFallbackEnabled() =
         GD.Print("_IsPlaceholderFallbackEnabled")
-        base._IsPlaceholderFallbackEnabled()
+        false
--- FSXScriptInterpreter/FsxScriptLanguage.fs
+++ FSXScriptInterpreter/FsxScriptLanguage.fs
@@ -6,41 +6,45 @@ open Godot.Collections
 
 type FsxScriptLanguage() =
     inherit ScriptLanguageExtension()
-    
+
     static member BindMethods(context: ClassDBRegistrationContext) =
         context.BindConstructor(fun () -> new FsxScriptLanguage())
-    
-    override this._Init() =
-        ()
+
+    override this._Init() = ()
+
     override _._CreateScript() =
         GD.Print("CreateScript")
         new FsxScript()
-    override _._GetName() =
-        "FsxScriptLanguage"
-    override this._GetType() =
-        "FsxScript"
-    override this._GetExtension() =
-        "fsx"
+
+    override _._GetName() = "FsxScriptLanguage"
+    override this._GetType() = "FsxScript"
+    override this._GetExtension() = "fsx"
+
     override this._Finish() =
         GD.Print("Finish")
-        base._Finish()
+        ()
+
     override this._GetReservedWords() =
         GD.Print("GetReservedWords")
         new PackedStringArray()
+
     override this._IsControlFlowKeyword(keyword) =
         GD.Print("IsControlFlowKeyword")
-        base._IsControlFlowKeyword(keyword)
-    override this._GetCommentDelimiters() =
-        new PackedStringArray(["//"])
+        false
+
+    override this._GetCommentDelimiters() = new PackedStringArray([ "//" ])
+
     override this._GetDocCommentDelimiters() =
         GD.Print("GetDocCommentDelimiters")
         new PackedStringArray()
+
     override this._GetStringDelimiters() =
         let list = new PackedStringArray()
         list.Add("\" \"")
         list.Add("' '")
         list.Add("@\" \"")
         list
+
     override this._MakeTemplate(template, className, baseClassName) =
         let code =
             $"""module {className}
@@ -53,96 +57,124 @@ type State = struct end
 
 let _process(self : Base) (delta: float) =
     ()"""
+
         let script = new FsxScript()
         script.SetSourceCode(code)
         script
-        
+
     override this._GetBuiltInTemplates(object) =
         GD.Print("GetBuiltInTemplates")
         new GodotArray<GodotDictionary>()
+
     override this._IsUsingTemplates() =
         GD.Print("IsUsingTemplates")
-        base._IsUsingTemplates()
+        false
+
     override this._Validate(script, path, validateFunctions, validateErrors, validateWarnings, validateSafeLines) =
         GD.Print("date")
         new GodotDictionary()
+
     override this._ValidatePath(path) =
         GD.Print("ValidatePath")
-        base._ValidatePath(path)
+        path
+
     override this._HasNamedClasses() =
         GD.Print("HasNamedClasses")
-        base._HasNamedClasses()
+        false
+
     override this._SupportsBuiltinMode() =
         GD.Print("SupportsBuiltinMode")
-        base._SupportsBuiltinMode()
+        false
+
     override this._SupportsDocumentation() =
         GD.Print("SupportsDocumentation")
-        base._SupportsDocumentation()
+        false
+
     override this._CanInheritFromFile() =
         GD.Print("CanInheritFromFile")
-        base._CanInheritFromFile()
+        false
+
     override this._FindFunction(_function, code) =
         GD.Print("FindFunction")
-        base._FindFunction(_function, code)
+        0
+
     override this._MakeFunction(className, functionName, functionArgs) =
         GD.Print("MakeFunction")
-        base._MakeFunction(className, functionName, functionArgs)
+        ""
+
     override this._CanMakeFunction() =
         GD.Print("CanMakeFunction")
-        base._CanMakeFunction()
+        false
+
     override this._OpenInExternalEditor(script, line, column) =
         GD.Print("OpenInExternalEditor")
-        base._OpenInExternalEditor(script, line, column)
+        Error.Unavailable
+
     override this._OverridesExternalEditor() =
         GD.Print("OverridesExternalEditor")
-        base._OverridesExternalEditor()
+        false
+
     override this._PreferredFileNameCasing() =
         GD.Print("PreferredFileNameCasing")
         Godot.ScriptLanguage.ScriptNameCasing.SnakeCase
+
     override this._CompleteCode(code, path, owner) =
         GD.Print("CompleteCode")
         new GodotDictionary()
+
     override this._LookupCode(code, symbol, path, owner) =
         GD.Print("LookupCode")
         new GodotDictionary()
+
     override this._AutoIndentCode(code, fromLine, toLine) =
         GD.Print("AutoIndentCode")
-        base._AutoIndentCode(code, fromLine, toLine)
+        code
+
     override this._AddGlobalConstant(name, value) =
         GD.Print("AddGlobalConstant")
-        base._AddGlobalConstant(name, value)
+        ()
+
     override this._AddNamedGlobalConstant(name, value) =
         GD.Print("AddNamedGlobalConstant")
-        base._AddNamedGlobalConstant(name, value)
+        ()
+
     override this._RemoveNamedGlobalConstant(name) =
         GD.Print("RemoveNamedGlobalConstant")
-        base._RemoveNamedGlobalConstant(name)
+        ()
+
     override this._ReloadAllScripts() =
         GD.Print("ReloadAllScripts")
-        base._ReloadAllScripts()
+        ()
+
     override this._ReloadToolScript(script, softReload) =
         GD.Print("ReloadToolScript")
-        base._ReloadToolScript(script, softReload)
-    override this._GetRecognizedExtensions() =
-        new PackedStringArray([|"fsx"|])        
+        ()
+
+    override this._GetRecognizedExtensions() = new PackedStringArray([| "fsx" |])
+
     override this._GetPublicFunctions() =
         GD.Print("GetPublicFunctions")
         new GodotArray<GodotDictionary>()
+
     override this._GetPublicConstants() =
         GD.Print("GetPublicConstants")
         new GodotDictionary()
+
     override this._GetPublicAnnotations() =
         GD.Print("GetPublicAnnotations")
         new GodotArray<GodotDictionary>()
+
     override this._HandlesGlobalClassType(_type) =
         GD.Print("HandlesGlobalClassType")
-        base._HandlesGlobalClassType(_type)
+        false
+
     override this._GetGlobalClassName(path) =
         GD.Print("_GetGlobalClassName")
         let dict = new GodotDictionary()
         dict["name"] <- "Test"
         dict["base_type"] <- "Node"
         dict
+
     override this._ThreadEnter() = ()
     override this._ThreadExit() = ()
-    override this._Frame() = base._Frame()
+    override this._Frame() = ()
